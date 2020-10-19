921a69be51721136826469566007ba510a019e3b
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-19 17:31:12 -0300

    Created configuration to expose the tasks ids
    Created configuration to not export the appUser in public

--- src/main/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplication.groovy
+++ src/main/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplication.groovy
@@ -1,13 +1,20 @@
 package br.com.ogawadev.bluetasksbackendgroovy
 
+import br.com.ogawadev.bluetasksbackendgroovy.domain.task.Task
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
 
 @SpringBootApplication
-class BluetasksBackendGroovyApplication {
+class BluetasksBackendGroovyApplication implements RepositoryRestConfigurer{
 
     static void main(String[] args) {
         SpringApplication.run(BluetasksBackendGroovyApplication, args)
     }
 
+    @Override
+    void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Task.class)
+    }
 }
