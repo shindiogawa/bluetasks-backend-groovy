1e8c1c52086c949d0216b4ca93a07551bbf6c27b
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-19 19:37:51 -0300

    Enabling logger in main application

--- src/main/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplication.groovy
+++ src/main/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplication.groovy
@@ -1,6 +1,8 @@
 package br.com.ogawadev.bluetasksbackendgroovy
 
 import br.com.ogawadev.bluetasksbackendgroovy.domain.task.Task
+import groovy.util.logging.Slf4j
+import org.apache.logging.slf4j.SLF4JLogger
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
@@ -11,10 +13,13 @@ import org.springframework.validation.Validator
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 
 @SpringBootApplication
+@Slf4j
 class BluetasksBackendGroovyApplication implements RepositoryRestConfigurer{
+
 
     static void main(String[] args) {
         SpringApplication.run(BluetasksBackendGroovyApplication, args)
+        log.info("Bluetasks in action")
     }
 
     @Override
@@ -32,5 +37,6 @@ class BluetasksBackendGroovyApplication implements RepositoryRestConfigurer{
         Validator validator = validator()
         validatingListener.addValidator("beforeCreate",validator)
         validatingListener.addValidator("beforeSave",validator)
+        log.info("Configure Validator ... OK!")
     }
 }
