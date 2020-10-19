8e4200f6942c07f4339cea56918566d0536d8892
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-19 10:55:33 -0300

    Created initial project structure

---
+++ src/main/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplication.groovy
@@ -0,0 +1,13 @@
+package br.com.ogawadev.bluetasksbackendgroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BluetasksBackendGroovyApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BluetasksBackendGroovyApplication, args)
+    }
+
+}
---
+++ src/test/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplicationTests.groovy
@@ -0,0 +1,13 @@
+package br.com.ogawadev.bluetasksbackendgroovy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BluetasksBackendGroovyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
