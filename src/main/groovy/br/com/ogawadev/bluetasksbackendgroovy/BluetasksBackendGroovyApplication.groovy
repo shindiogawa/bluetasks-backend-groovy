ab34b63f1cd9376e8caac368542e590d1f352a12
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-10-19 18:11:09 -0300

    Created validation error message get/post process

--- src/main/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplication.groovy
+++ src/main/groovy/br/com/ogawadev/bluetasksbackendgroovy/BluetasksBackendGroovyApplication.groovy
@@ -3,8 +3,12 @@ package br.com.ogawadev.bluetasksbackendgroovy
 import br.com.ogawadev.bluetasksbackendgroovy.domain.task.Task
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.validation.Validator
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 
 @SpringBootApplication
 class BluetasksBackendGroovyApplication implements RepositoryRestConfigurer{
@@ -16,5 +20,17 @@ class BluetasksBackendGroovyApplication implements RepositoryRestConfigurer{
     @Override
     void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Task.class)
+    }
+
+    @Bean
+    Validator validator() {
+        return new LocalValidatorFactoryBean()
+    }
+
+    @Override
+    void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+        Validator validator = validator()
+        validatingListener.addValidator("beforeCreate",validator)
+        validatingListener.addValidator("beforeSave",validator)
     }
 }
